6b24351b907568c5eec92b8266f7493e0b825115
Nobil Gautam <nobilgautam007@gmail.com>
2022-04-14 23:48:09 +0530

    questionBank array declaration fixed, ClickListener added to next and prev buttons

--- app/src/main/java/com/example/truecitizen/MainActivity.java
+++ app/src/main/java/com/example/truecitizen/MainActivity.java
@@ -4,28 +4,43 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.truecitizen.databinding.ActivityMainBinding;
 import com.example.truecitizen.model.Questions;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
+    int curr = 0;
+    Questions questionBank[] = new Questions[] {
+            new Questions(R.string.father_of_nation, true),
+            new Questions(R.string.year_of_independence, false),
+            new Questions(R.string.republic_day, false),
+            new Questions(R.string.oldest_civilization, false)
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-        Questions questionBank[] = new Questions[] {
-                new Questions(R.string.father_of_nation, true),
-                new Questions(R.string.year_of_independence, false),
-                new Questions(R.string.republic_day, false),
-                new Questions(R.string.oldest_civilization, false)
-        };
 
-        int curr = 0;
+
+
         binding.QuestionTextView.setText(questionBank[curr].getQuesId());
 
+        binding.NextButton.setOnClickListener(v -> {
+            curr = (curr + 1) % questionBank.length;
+            binding.QuestionTextView.setText(questionBank[curr].getQuesId());
+        });
+
+        binding.PrevButton.setOnClickListener(v -> {
+            curr--;
+            if (curr == 0) {
+                curr = questionBank.length;
+            }
+            binding.QuestionTextView.setText(questionBank[curr].getQuesId());
+        });
 
     }
 }
