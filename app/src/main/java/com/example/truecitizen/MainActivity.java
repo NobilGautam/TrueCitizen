6a2b18c007e54ff2df093a6219d20399f28f5ce4
Nobil Gautam <nobilgautam007@gmail.com>
2022-04-14 23:39:11 +0530

    Question initiated on screen

--- app/src/main/java/com/example/truecitizen/MainActivity.java
+++ app/src/main/java/com/example/truecitizen/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.databinding.DataBindingUtil;
 import android.os.Bundle;
 
 import com.example.truecitizen.databinding.ActivityMainBinding;
+import com.example.truecitizen.model.Questions;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
@@ -14,6 +15,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        Questions questionBank[] = new Questions[] {
+                new Questions(R.string.father_of_nation, true),
+                new Questions(R.string.year_of_independence, false),
+                new Questions(R.string.republic_day, false),
+                new Questions(R.string.oldest_civilization, false)
+        };
+
+        int curr = 0;
+        binding.QuestionTextView.setText(questionBank[curr].getQuesId());
+
 
     }
 }
